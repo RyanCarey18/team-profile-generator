bd8e83b056a5bed7d98094c2cec909b74508c721
Ryan Carey <ryaanski18@yahoo.com>
2021-11-14 20:30:46 -0500

    added to node_modules to gitignore, created package.json, created inquirer prompt for manager qs

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const Employee = require("./lib/employee");
 const inquirer = require("inquirer");
+
+const managerQs = [
+  {
+    type: "input",
+    message: "What is the Team Managers name?",
+    name: "manager",
+  },
+  {
+    type: "input",
+    message: "What is the Team Managers ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the Team Managers Email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the Team Managers office number?",
+    name: "officeNumber",
+  },
+];
+
+function init() {
+  inquirer.prompt(managerQs).then((response) => {
+    console.log(response);
+  });
+}
+
+init();
